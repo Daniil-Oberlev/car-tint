40e5f76461957720c4b0c36bb7fa3e6224ea8762
Daniil Oberlev <denioberlev@gmail.com>
2024-12-01 08:49:48 +0300

    -extra

--- src/env/routes.ts
+++ src/env/routes.ts
@@ -3,7 +3,6 @@ const routes = {
   BACKEND_REPOSITORY: process.env.BACKEND_REPOSITORY as string,
   AI_REPOSITORY: process.env.AI_REPOSITORY as string,
 
-  MAP_API_KEY: process.env.MAP_API_KEY as string,
   PROCESS_IMAGE: process.env.PROCESS_IMAGE as string,
 };
 
